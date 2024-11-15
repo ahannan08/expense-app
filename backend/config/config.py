b56d1178a7b8daf33fbe99822b9ac4811bf25df0
a.hannan08 <mohdabdulhannan08@gmail.com>
2024-11-15 17:45:23 +0530

    db

--- backend/config/config.py
+++ backend/config/config.py
@@ -1,7 +1,6 @@
-from flask_sqlalchemy import SQLAlchemy
+import os
 
-
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:messi10@localhost:5432/budget'
-SQLALCHEMY_TRACK_MODIFICATIONS= False
-
-
+# Use the DATABASE_URL from Heroku environment variable if it exists;
+# otherwise, default to the local database URL.
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://postgres:messi10@localhost:5432/budget')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
